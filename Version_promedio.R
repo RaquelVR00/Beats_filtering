dc438ff3697a3c520eab969846c1b52bd07032af
RaquelVR00 <61518082+RaquelVR00@users.noreply.github.com>
2021-05-08 20:34:27 +0200

    Add files via upload

--- Version_promedio.R
+++ Version_promedio.R
@@ -2,7 +2,7 @@ library("lubridate")
 #Se llamaran a los registros dentro del bucle for para ir aumentando el numero
 #del registro que se quiere analizar
 
-change<-c(0.1,0.15,0.25,0.2,0.25,0.30,0.35,0.40,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1,2,3)
+change<-c(0.1,0.15,0.2,0.25,0.30,0.35,0.40,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1,2,3)
 #Matriz para sumatorio
 
 vector_sensibilidad=vector_especificidad=rep('0',length(change))
